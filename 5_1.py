ea08ec9c45caf820e3317bc44eddd3d1c293ae90
Dimonceg95 <dimonzinew@gmail.com>
2023-05-16 13:29:32 +0300

    добавил 5.1

---
+++ 5_1.py
@@ -0,0 +1,30 @@
+# 1. Написать класс Car
+# Конструктор класса принимает атрибуты:
+# color: str (цвет)
+# count_passenger_seats: int (количество пассажирских мест)
+# is_baby_seat: bool (наличие десткого кресла)
+# is_busy: bool (определяется внутри конструктора со значением False, не принимается на
+# вход)
+# 1.1 Написать магический метод __str__ выводящий форматированную строку с информацией
+# об автомобиле
+class Button:
+
+    def __init__(self, text: str, link:str) -> None:
+        self.text = text
+        self.link = link
+
+    def dict(self) -> dict:
+        return {'text': self.text, 'link': self.link}
+class Car:
+
+    def __init__(self, color: str, count_passenger_seats: int, is_baby_seat: bool):
+        self.is_busy = False
+        self.color = color
+        self.count_passenger_sears = count_passenger_seats
+        self.is_baby_seat = is_baby_seat
+
+    def __str__(self) -> str:
+        return f'color: {self.color}, count passenger seats: {self.count_passenger_sears}, is_baby_seat:' \
+               f' {self.is_baby_seat}, is_busy: {self.is_busy}'
+mersedes = Car('red', 5, True)
+print(mersedes)
